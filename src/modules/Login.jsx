d956778965a2eeee37ee283d04f216cffb90c0ed
ReneBta <ivanbautista511@gmail.com>
2022-01-14 16:44:22 -0600

    Configuracion inicial del proyecto

---
+++ src/modules/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    //funciones de preferencia que sean flecha
+    return (
+        //es donde se regresan los elementos HTML 
+        <>
+        <h1>Gestor</h1>
+        </>
+    )
+}
+
+export default Login
